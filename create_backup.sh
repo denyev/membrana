5a1bfdcd6a39b74e0493b0b0dfe3ac85ed5c35b1
denyev <denis.yevtushenko@yandex.ru>
2018-07-21 18:19:42 +0300

    Improves the script to create a backup

--- create_backup.sh
+++ create_backup.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env bash
 
-nice -n 19 ionice -c3 tar -czvf ${HOME}/_Backups/membrana/membrana_2018-07-16-1.tgz ../membrana --exclude=node_modules --exclude=.idea --exclude=.git --exclude='*.psd' --exclude='*lock*'
+BACKUP_NAME="membrana"
+BACKUP_PATH="${HOME}/_Backups/${BACKUP_NAME}"
+CURRENT_DATE=$(date +"%F")
+COUNT=$(ls -1 ${BACKUP_PATH}/${BACKUP_NAME}_${CURRENT_DATE}* | wc -l )
+
+nice -n 19 ionice -c3 \
+tar -czvf ${BACKUP_PATH}/${BACKUP_NAME}_${CURRENT_DATE}_$((${COUNT} + 1)).tgz ../${BACKUP_NAME} \
+--exclude=node_modules --exclude=.idea --exclude=.git --exclude='*.psd' --exclude='*lock*'
