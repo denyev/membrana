2d2932f660d13a5baa004940911421e508b89773
denyev <denis.yevtushenko@yandex.ru>
2018-08-06 10:08:02 +0300

    Changes the path for the project

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 nice -n 19 ionice -c3 rsync  --progress --stats --bwlimit=1000 -e="ssh -p 22" -aAhHvX \
 ${HOME}/projects/membrana/dist/public/* \
-denyev@vh10.timeweb.ru:~/landings/public_html/
+denyev@vh10.timeweb.ru:~/landings/public_html/membrana/dist/public/
